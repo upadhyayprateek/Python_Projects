1b8e6b37be28b54cc1b0d6782bdf1da488c416cd
upadhyayprateek <2019ume1715@mnit.ac.in>
2021-07-15 14:32:20 +0530

    Second Commit

--- Trendingnews.py
+++ Trendingnews.py
@@ -1,8 +1,10 @@
 from GoogleNews import GoogleNews
 googlenews= GoogleNews()
 googlenews= GoogleNews(period="7d")
-googlenews.search('India')
-result= googlenews.result()
+topic = input("Enter the news you want to know about")
+googlenews.search(topic)
+result = googlenews.result()
+print(topic+" News")
 for x in result:
  print("-"*50)
  print("Title--", x['title'])
