dad48a2cbe5ad99e782f40e63ee4565dc09bd507
upadhyayprateek <2019ume1715@mnit.ac.in>
2021-07-15 15:10:21 +0530

    Third Commit

--- Trendingnews.py
+++ Trendingnews.py
@@ -1,11 +1,21 @@
+def readnews(topic,page):
+ googlenews.get_page(page)
+ result = googlenews.page_at(page) 
+ print(topic+" News")
+ for x in result:
+  print("-"*50)
+  print("Title--", x['title'])
+  # print("Image--", x['img'])
+  print("Description--", x['desc'])
+ val = input("Do you want to know more:(Answer in Y Or N) ")
+ if val == 'Y':
+  readnews(topic,page+1) 
+
 from GoogleNews import GoogleNews
 googlenews= GoogleNews()
 googlenews= GoogleNews(period="7d")
 topic = input("Enter the news you want to know about")
 googlenews.search(topic)
 result = googlenews.result()
-print(topic+" News")
-for x in result:
- print("-"*50)
- print("Title--", x['title'])
- print("Description--", x['desc'])
+page = 1
+readnews(topic,page)
